761848dec1325a831f50d2c2acd8515d0e8b055e
Squ1dd13 <squ1dd13dev@gmail.com>
2020-08-11 10:49:03 +0100

    Removed more unused parser components

--- src/com/squ1dd13/msd/compiler/text/parser/Assignment.java
+++
@@ -1,9 +0,0 @@
-package com.squ1dd13.msd.compiler.text.parser;
-
-import com.squ1dd13.msd.shared.*;
-
-public class Assignment {
-    public LowLevelType leftType, rightType;
-
-
-}
--- src/com/squ1dd13/msd/compiler/text/parser/CanBeCompilable.java
+++
@@ -1,7 +0,0 @@
-package com.squ1dd13.msd.compiler.text.parser;
-
-import com.squ1dd13.msd.compiler.*;
-
-public interface CanBeCompilable {
-    Compilable toCompilable();
-}
